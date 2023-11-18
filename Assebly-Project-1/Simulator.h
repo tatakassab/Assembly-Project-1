344c349c611b042f5f1a9c980b9ba441b0e53bd5
ShazaAli <107041989+ShazaAli@users.noreply.github.com>
2023-11-18 16:12:01 +0200

    Added the function headers and the registers' names array
    Added the function headers and the registers' names array to be printed

--- Assebly-Project-1/Simulator.h
+++ Assebly-Project-1/Simulator.h
@@ -7,6 +7,7 @@
 #include <vector>
 #include <map>
 #include <string>
+
 using namespace std;
 
 class Simulator
@@ -16,6 +17,9 @@ public:
 	void getInstructions();
 	void execInstructions();
 	void execute(vector<string> instruction);
+	void output();
+	string DecToHex(int output);
+	string DecToBin(int output);
 
 private:
 	unsigned int PC;
@@ -24,6 +28,7 @@ private:
 	vector<string> instructions;
 	string file;
 	string data;
+	string Regnames[32] = { "zero", "ra", "sp", "gp", "tp","t0","t1","t2","s0","s1","a0","a1","a2","a3","a4","a5","a6","a7","s2","s3","s4","s5","s6","s7","s8","s9","s10","s11","t3","t4","t5","t6" };
 };
 
 #endif // !SIMULATOR
