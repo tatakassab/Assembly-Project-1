36733b5dd58e194a61a18980d782ec1ca8d82989
Tarek Mohamed Kassab <tatakassab@gmail.com>
2023-11-19 20:26:20 +0200

    Validation and structure done

--- Assebly-Project-1/Simulator.h
+++ Assebly-Project-1/Simulator.h
@@ -13,19 +13,19 @@ class Simulator
 {
 public:
 	Simulator(string i_file, string i_data);
+
+private:
 	void getInstructions();
 	bool validate();
 	void execInstructions();
 	void execute(vector<string> instruction);
 	void setPC(unsigned int);
 	unsigned int getPC();
-	void incrementPC(int);
+	void incrementPC();
 	void incrementSP(int);
-	void setRegister(string, int); //takes in a string X1, or $1, or $t1
+	void setRegister(string, int); //takes in a string X1, or T1
 	int getRegister(string); //takes in a string X1, or $1, or $t1
 	void returnError(string); //Error handling
-
-private:
 	unsigned int PC;
 	unsigned int InitialPC;
 	map<unsigned int, int> Memory;
@@ -42,8 +42,12 @@ private:
 	vector<string> SType = { "SW", "SB", "SH" };
 	vector<string> SBType = { "BEQ", "BNE", "BLT", "BGE", "BLTU", "BGEU"};
 	vector<string> RType = { "ADD", "SUB", "SLL", "SLT", "SLTU", "XOR", "SRL", "SRA", "OR", "AND"};
+	vector<string> RegNames = { "ZERO", "RA", "SP", "GP", "TP", "T0", "T1", "T2", "S0", "S1", "A0", "A1", "A2", "A3", "A4", "A5", "A6", "A7", "S2", "S3", "S4", "S5", "S6", "S7", "S8", "S9", "S10", "S11", "T3", "T4", "T5", "T6" };
 	bool isInt(string);
 	bool isReg(string);
+	bool isFunc(string);
+	unsigned int stack_upper = 8912896; //0x880000
+	unsigned int stack_lower = 8388608; //0x800000
 };
 
 #endif // !SIMULATOR
